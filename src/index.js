c0d5e234dd86bfbafca98db6c3d92d0a2cddc0bb
sruti-sucharita <ece.22bech07@silicon.ac.in>
2024-08-04 09:13:04 +0530

    src/index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {BrowserRouter} from "react-router-dom";
-import Login from './Login';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter basename={window.location.pathname || ''}>
-     <Login/>
+     <App/>
     </BrowserRouter>
   </React.StrictMode>
 );
